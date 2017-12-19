bf4f983c7cad3385e00fab6ac21ef9c962f03cc8
Prajay Ghaghda <prajay.ghaghda@smartology.net>
2017-12-19 17:27:44 +0000

    Added recursive matching function

--- mills.py
+++ mills.py
@@ -1,15 +1,12 @@
 
 import random
 
-ulf = {'Adam':'M', 'Alex M':'M', 'Anisa':'F' , 'Bertie':'M', 'Hannah':'F', 'Laura':'F', 'Nikki':'F',
+ulf = {'Adam':'M', 'Alex M':'M', 'Anisa':'F', 'Bertie':'M', 'Hannah':'F', 'Laura':'F', 'Nikki':'F',
  'Pete':'M', 'Robin':'M', 'Zoe':'F', 'Fiona':'F', 'Kat':'F', 'Emma R':'F',
 'Flora':'F', 'Louisa':'F', 'Emma V':'F', 'Harry':'M', 'Darryl':'M', 'Prajay':'M'}
 
 
-characters = {'Jason': 'M', 'Jason Lombard': 'M', 'Sophie Melville': 'F', 'Sophie': 'F', 'Mother': 'F', 'Sophie':'F',
-	'Mr Lombard': 'M', 
-
-}
+characters = {'Jason': 'M', 'Jason Lombard': 'M', 'Sophie Melville': 'F', 'Sophie': 'F', 'Mother': 'F', 'Sophie':'F','Mr Lombard': 'M'}
 
 def gatherText():
 	with open("D:/Prajay/2017/Q4/in-house/mills/MillsBoon/texts/Emma Darcy- A Very Stylish Affair.txt") as f:
@@ -17,7 +14,7 @@ def gatherText():
 		corpus = ''
 		for x in range(0,100):
 			corpus = corpus + text[x]
-	
+	# Find a way to split into chapters.
 
 	# print corpus.replace("Jason", "Adam")
 	replacement = random.choice(list(ulf.keys()))
@@ -27,20 +24,24 @@ def gatherText():
 		print 'Nothing found' 
 	print replacement
 	print ulf[replacement]
-
-
-
+	print match
 
 
 def findMatch():
 	ulfer = random.choice(list(ulf))
 	char = random.choice(list(characters))
+	replacements = []
 	if ulf[ulfer] == characters[char]:
-		return ulfer + char
+		replacements.append(ulfer)
+		replacements.append(char)
+		return replacements
 	else: 
-		findMatch()
+		return findMatch()
+|# Consider returning more than one match in the list that comes back?
 
 	
 
 match = findMatch()
-print match
+
+
+gatherText()
